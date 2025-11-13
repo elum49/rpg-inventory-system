5a3cd6e9b830fd78f58bc91fc4d8ea67c78de9dd
elume <staspovetko166@gmail.com>
2025-11-13 13:08:48 +0300

    add basic project structure and Item struct

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cstring>
 
+struct Item {
+    char* name;
+    char* type;
+    int level;
+};
+
+char* copyString(const char* source) {
+    char* copy = new char[strlen(source) + 1];
+    strcpy(copy, source);
+    return copy;
+}
+
+int main() {
+    std::cout << "RPG Inventory System" << std::endl;
+    return 0;
+}
